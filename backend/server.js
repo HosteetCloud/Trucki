c5a7fe6637b1e11181f9f95cc112a363f1b149c6
hokageCodes <hashsenju94@gmail.com>
2023-06-01 01:56:56 +0100

    Restart

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose')
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+// Routes
+// app.use('/api/auth', require('./routes/auth'));
+
+mongoose
+    .connect("mongodb+srv://Hosteet:Trucki%402023@cluster0.5aikql0.mongodb.net/?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Failed to connect to MongoDB:', error));
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
